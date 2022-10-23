a015c533420836df040ebcf0317febb0f27379ac
kandiana <dianka95@gmail.com>
2022-10-23 23:45:13 +0300

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,14 @@
 
 git branch -D ghpages
 git checkout --orphan ghpages
-git rm -rf .
 
 ng build --deploy-url '/itlogia_intensive/' --base-href '/itlogia_intensive/'
 mkdir deploy
 cp -r ./dist/agproject/* ./deploy/
 
+git rm -rf .
 git add deploy
 git commit -m 'built project'
 git push -uf origin ghpages
+
 git switch master
